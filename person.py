ce779ccbc1954adc958e129007d37a133f0ae5d4
Evan Richter <evan@evanhere.com>
2015-04-23 23:52:49 -0500

    Added Stuff

---
+++ person.py
@@ -0,0 +1,41 @@
+#Created by Evan Richter
+
+class Person: 
+
+    def __init__(self):
+    	self.name = ""
+        self.position = ""
+        self.location = ""
+        self.industry = ""
+        self.image = ""
+
+        """
+
+    def getTitle(self):
+    	return self.title
+    def setTitle(self, newtitle):
+    	self.title = newtitle
+
+    def getArtist(self):
+        return self.artist
+    def setArtist(self, newartist):
+        self.artist = newartist
+
+    def getLength(self):
+        return self.length
+    def setLength(self, length):
+        self.length = length
+
+    def getUri(self):
+        return self.uri
+    def setUri(self, newUri):
+        self.uri = newUri
+
+    def getId(self):
+        return self.id
+    def setId(self, newid):
+        self.id = newid
+"""
+
+
+
